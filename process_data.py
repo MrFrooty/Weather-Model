820f318b75d484bb6c610e5a60628e5818e3d51f
freddy <fredsong99@gmail.com>
2023-03-31 21:17:42 -0700

    Processed old data into new CSV file

---
+++ process_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#Read file using pandas library
+df = pd.read_csv('rainfall in india 1901-2015.csv')
+
+#Clean up data set to only include year number and the 12 months as the columns and the data values from 
+#the MADHYA MAHARASHTRA subdivision. This is the 2nd largest state in India
+df = df.drop(['index', 'SUBDIVISION','ANNUAL', 'Jan-Feb', 'Mar-May', 'Jun-Sep', 'Oct-Dec'], axis=1)
+df = df.loc[2622:2736]
+
+#Process the data into a new csv file
+df.to_csv('rainfall in india processed.csv', index=False)
