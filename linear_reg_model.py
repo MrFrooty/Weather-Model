9da8782f8259b2ba114a5d3a5f6864b7479fc06e
freddy <fredsong99@gmail.com>
2023-04-02 12:59:43 -0700

    Created Linear Regression Model and cleaned up code.

---
+++ linear_reg_model.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.preprocessing import PolynomialFeatures
+
+#Load dataset into file
+df = pd.read_csv('rainfall in india processed.csv')
+
+#Create correct datavalues
+years = df.iloc[:, 0].values.reshape(-1,1)
+rainfall = df.iloc[:, 1:].values
+
+# Calculate the train size as 80% of the len of df and then finding that value as the nearest multiple of 12 (seasonal pattern)
+train_size = int(len(df) * 0.8) // 12 * 12
+
+# Split the data into training and test sets
+years_train, rainfall_train = years[:train_size], rainfall[:train_size]
+years_test, rainfall_test = years[train_size:], rainfall[train_size:]
+
+models = {}
+
+#Train an individual model for each month using ever year's dataset
+for month in range(12):
+    lin_reg = LinearRegression()
+    lin_reg.fit(years, rainfall[:, month])
+    models[month] = lin_reg
+    # multi_poly = PolynomialFeatures(degree=2)
+    # years_poly = multi_poly.fit_transform(years_train)
+    # #multi_poly.fit(years_poly, rainfall[:, month])
+    # lin_reg_multi = LinearRegression()
+    # lin_reg_multi.fit(years_poly, rainfall_train)
+    # models[month] = lin_reg_multi
+    # rainfall_preds = lin_reg_multi.predict(multi_poly.fit_transform(years_test))
+
+#Predict the values for each month and output into an array
+rainfall_preds = np.zeros_like(rainfall_test)
+for month in range(12):
+    model = models[month]
+    rainfall_preds[:, month] = model.predict(years_test)
+
+#First calculate the mse based on predicted and test values and output to an array
+#Loop through that array while keeping track of the index and print out error in correspondence to the correct month
+mse = metrics.mean_squared_error(rainfall_test, rainfall_preds, multioutput='raw_values')
+for month, error in enumerate(mse):
+    print(f"MSE for Month {month+1}: {error:.2f}")
+    
+fig, axs = plt.subplots(ncols = 4, nrows = 3, figsize=(12,9))
+fig.suptitle('Error difference between predicted and test values')
+for month in range(12):
+    row = month//4
+    col = month%4
+    ax = axs[row][col]
+    ax.plot(years_test, rainfall_test[:, month], label= 'Actual', color='blue')
+    ax.plot(years_test, rainfall_preds[:, month], label = 'Predicted', color='red')
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Rainfall (inches)")
+    ax.set_title(f"Month {month + 1}")
+    
+fig.subplots_adjust(hspace=0.5, wspace=0.3)
+    
+plt.show()
--- process_data.py
+++ process_data.py
@@ -9,22 +9,5 @@ df = pd.read_csv('rainfall in india 1901-2015.csv')
 df = df.drop(['index', 'SUBDIVISION','ANNUAL', 'Jan-Feb', 'Mar-May', 'Jun-Sep', 'Oct-Dec'], axis=1)
 df = df.loc[2622:2736]
 
-# df["Date"] = pd.to_datetime(df["Year"].astype(str) + df["Month"].astype(str), format="%Y%m")
-# df = df.set_index("Date")
-# df = df.drop(["Year", "Month"], axis=1)
-
-# df = df.stack().reset_index()
-# df.columns = ["Date", "Rainfall"]
-
-# df["Year"] = df["Date"].dt.year
-# df["Month"] = df["Date"].dt.month
-
-# df = df.drop("Date", axis=1)
-
-# X = df[["Year", "Month"]]
-# y = df["Rainfall"]
-
-# print(X, y)
-
 #Process the data into a new csv file
 df.to_csv('rainfall in india processed.csv', index=False)
