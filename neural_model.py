3da168414825613e533125310c781320a9770587
freddy <fredsong99@gmail.com>
2023-04-02 11:10:56 -0700

    Modified CSV file

--- neural_model.py
+++ neural_model.py
@@ -8,15 +8,7 @@ from keras.preprocessing.sequence import pad_sequences
 
 weather_data = pd.read_csv('rainfall in india processed.csv')
 
-years = weather_data["YEAR"].values
-months = [1,2,3,4,5,6,7,8,9,10,11,12]
-x = []
-for year in years:
-    year_array = []
-    for month in months:
-        year_array.append(f"{month}/{year}")
-    x.append(year_array)
-
+x = weather_data["YEAR"].values
 weather_data = weather_data.drop(columns=["YEAR"])
 y = weather_data.iloc[:, :].values
 
